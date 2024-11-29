55f78023d257292d751c2c0603427bf8d40cdeeb
Joscha <34318751+josxha@users.noreply.github.com>
2024-11-30 00:34:59 +0100

    add attribution

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,15 +1,16 @@
 import {
+  AttributionControl,
   FullscreenControl,
   GeolocateControl,
   Map,
   NavigationControl,
   ScaleControl,
 } from 'react-map-gl/maplibre';
-import { Protocol } from "pmtiles";
+import {Protocol} from "pmtiles";
 import maplibregl from 'maplibre-gl';
 
 export function MapView() {
-  const protocol = new Protocol()
+  const protocol = new Protocol({metadata: true})
   maplibregl.addProtocol('pmtiles', protocol.tile)
 
   return (
@@ -21,6 +22,7 @@ export function MapView() {
           zoom: 9,
         }}
         maxPitch={80}
+        attributionControl={false}
         style={{width: "100%", height: "93.5623vh"}}
         mapStyle="https://raw.githubusercontent.com/OpenSeaCharts/osc-style/refs/heads/main/openseacharts.json"
       >
@@ -29,6 +31,12 @@ export function MapView() {
         <GeolocateControl trackUserLocation={true} showAccuracyCircle={true}
                           showUserLocation={true}/>
         <ScaleControl unit={"metric"}/>
+        <AttributionControl
+          customAttribution={[
+            "<a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a>"
+          ]}
+          compact={true}
+        />
       </Map>
     </>
   );
